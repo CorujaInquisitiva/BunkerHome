afa245b5845311307a55cb9680db36ca693ba748
CorujaInquisitiva <isadoraalo@protonmail.com>
2026-02-10 09:17:23 -0300

    Personagens na tela e alteração da pasta assets.

--- src/app/games/warhammer/warhammer.ts
+++ src/app/games/warhammer/warhammer.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { AppConstants } from '../shared/utils/constants/warhammer.constants';
 
 @Component({
   selector: 'warhammer-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './warhammer.html',
   styleUrl: './warhammer.css',
 })
 export class Warhammer {
+personagens = AppConstants.THUMBS;
+
+
 
 }
