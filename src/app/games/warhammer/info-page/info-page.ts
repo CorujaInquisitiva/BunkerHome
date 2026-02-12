672f8b782bf0d0b38653ad1616b3b9866027b061
CorujaInquisitiva <isadoraalo@protonmail.com>
2026-02-11 23:01:41 -0300

    Página de informações

--- src/app/games/warhammer/info-page/info-page.ts
+++ src/app/games/warhammer/info-page/info-page.ts
@@ -1,4 +1,7 @@
 import { Component, Input } from '@angular/core';
+import { WarhammerConstants } from '../../shared/utils/constants/warhammer.constants';
+import { PersonagemId } from '../../shared/utils/types/warhammer.types';
+import { NgClass } from "../../../../../node_modules/@angular/common/types/_common_module-chunk";
 
 @Component({
   selector: 'info-page',
@@ -8,9 +11,11 @@ import { Component, Input } from '@angular/core';
 })
 export class InfoPage {
   @Input() personagemAtivo: string = '';
-
   get backgroundUrl(): string {
     if (!this.personagemAtivo) return 'none';
     return `url(assets/warhammer/fundos/${this.personagemAtivo}.png)`;
+  }
+  get infoText(): string {
+    return WarhammerConstants.INFO[this.personagemAtivo as PersonagemId] + '<br><br><br>';
   }
 }
