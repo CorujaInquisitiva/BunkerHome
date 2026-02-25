eef05029e427b5185e5c8677b96e07adba942554
CorujaInquisitiva <isadoraalo@protonmail.com>
2026-02-25 10:27:01 -0300

    Corrigindo posicionamento do menu

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, output, signal } from '@angular/core';
+import { Component, HostListener, OnInit, output, signal } from '@angular/core';
 import { GAMES_COMPONENTS } from './games/games-standalone';
 import { FoundryStatus } from './games/shared/models/foundry.model';
 import { FoundryStatusService } from './games/shared/services/FoundryService.service';
@@ -11,24 +11,32 @@ import { SistemaEnum } from './shared/utils/enums/sistemas.enums';
   styleUrl: './app.css',
 })
 export class App implements OnInit {
-
   constructor(readonly service: FoundryStatusService) {}
-
   personagemAtivo?: string | null = undefined;
 
   onPersonagemChange(personagem: any) {
-    console.log(this.personagemAtivo);
     this.personagemAtivo = personagem;
   }
 
   sistemaAtual = signal<string | null>(null);
 
   sistemas = SistemaEnum;
+  get isLargeScreen(): boolean {
+    return window.innerHeight >= 601;
+  }
+  get menuStyle() {
+    if (this.sistemaAtual() === this.sistemas.WFRP4E) {
+      return { 'padding-top': this.isLargeScreen ? '80px' : '40px' };
+    }
+    return { 'padding-top': this.isLargeScreen ? '12%' : '7%' };
+  }
 
+  @HostListener('window:resize')
   ngOnInit(): void {
     this.service.getStatus().subscribe({
       next: (response: FoundryStatus) => {
         this.sistemaAtual.set(response.system);
+        this.menuStyle;
       },
       error: (err: any) => {
         console.error(err);
--- src/app/games/shared/components/menu/menu.ts
+++ src/app/games/shared/components/menu/menu.ts
@@ -1,13 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'menu-view',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './menu.html',
   styleUrl: './menu.css',
 })
 export class Menu {
   @Input() showMenu: boolean = true;
+  @Input() style?: { [key: string]: string };
   menuItems = [
     { id: 'blog', icon: 'assets/warhammer/menu/Blog.png', link: 'https://blog.1nodado.com.br/' },
     { id: 'foundry', icon: 'assets/warhammer/menu/Foundry.png', link: 'https://foundry.1nodado.com.br/join' },
